8d126e20741c65e6b03f28686f5306346734b7f9
tatsuya828828 <kkjy011894@gmail.com>
2020-07-10 13:12:22 +0900

    [finith]06-06

---
+++ src/main/Calc.java
@@ -0,0 +1,12 @@
+package main;
+import java.util.Arrays;
+
+public class Calc {
+	public static void main(String[] args) {
+		int[] heights = {172, 149, 152, 191, 155};
+		Arrays.sort(heights);
+		for(int h: heights) {
+			System.out.println(h);
+		}
+	}
+}
