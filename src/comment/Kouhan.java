96b03e96f4560eba6a8f517d3d4b2633e59f06b9
tatsuya828828 <kkjy011894@gmail.com>
2020-07-10 13:27:42 +0900

    [finish]06-08

---
+++ src/comment/Kouhan.java
@@ -0,0 +1,14 @@
+package comment;
+
+public class Kouhan {
+	public static void callDeae(){
+		System.out.println("えぇい、こしゃくな。曲者だ！出会えい！");
+	}
+
+	public static void showMondokoro() throws Exception {
+		System.out.println("飛車さん、角さん。もういいでしょう。");
+		System.out.println("この紋所が目に入らぬか！");
+		Thread.sleep(3000);
+		Zenhan.doTogame();
+	}
+}
---
+++ src/main/Main.java
@@ -0,0 +1,13 @@
+package main;
+
+import comment.Kouhan;
+import comment.Zenhan;
+
+public class Main {
+	public static void main(String[] args) throws Exception {
+		Zenhan.doWarusa();
+		Zenhan.doTogame();
+		Kouhan.callDeae();
+		Kouhan.showMondokoro();
+	}
+}
